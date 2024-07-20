5e7e6f911898f42d948ddcaf802043777faee4fe
Yahyasyah <yahyaalamsyah02@gmail.com>
2024-07-21 03:26:14 +0700

    first commit

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,64 +1,43 @@
-// eslint-disable-next-line no-undef
 describe('Categories Menu', () => {
-    // eslint-disable-next-line no-undef
-    it('should navigate to Categories page and perform CRUD operations', () => {
-      // eslint-disable-next-line no-undef
-      cy.visit('http://localhost:5173/')
-      // eslint-disable-next-line no-undef
-      cy.wait(3000); // Tunggu 3 detik setelah mengunjungi halaman
-  
-      // eslint-disable-next-line no-undef
-      cy.get('[data-testid="form"]').should("exist")
-  
-      // eslint-disable-next-line no-undef
-      cy.get('input#email')
-        .should('be.visible')
-        .should('have.attr', 'placeholder', 'Enter your email')
-        .type('admin@store.com').should('have.value', 'admin@store.com')
-  
-      // eslint-disable-next-line no-undef
-      cy.get('input#password')
-        .should('be.visible')
-        .should('have.attr', 'placeholder', 'Enter your password')
-        .type('123456').should('have.value', '123456') 
-        
-      // eslint-disable-next-line no-undef
-      cy.get('[data-testid="submit"]').click()
-      // eslint-disable-next-line no-undef
-      cy.wait(3000); // Tunggu 3 detik setelah klik submit
-  
-      // eslint-disable-next-line no-undef
-      cy.get('div.home').should('be.visible')
-      // eslint-disable-next-line no-undef
-      cy.get('div.sidebar').should('be.visible')
-  
-      // 1) Mengklik menu Categories dan memastikan masuk ke halaman Categories
-      // eslint-disable-next-line no-undef
-      cy.get('[data-testid="categories"]', { timeout: 10000 }).should('be.visible').click()
-      // eslint-disable-next-line no-undef
-      cy.wait(3000); // Tunggu 3 detik setelah mengklik Categories
-      // eslint-disable-next-line no-undef
-      cy.url().should('include', '/categories')
-      // eslint-disable-next-line no-undef
-      cy.get('div.datatableTitle').contains('CATEGORIES')
-  
-      // 2) Klik tombol Add New 
-      // eslint-disable-next-line no-undef
-      cy.get('[data-testid="add-new"]').click() // Ganti selector sesuai dengan tombol Add New
-      // eslint-disable-next-line no-undef
-      cy.wait(2000); // Tunggu 3 detik setelah mengklik Add New
+  it('should navigate to Categories page and perform CRUD operations', () => {
+    cy.visit('http://localhost:5173/')
+    cy.wait(3000); // Tunggu 3 detik setelah mengunjungi halaman
+
+    cy.get('[data-testid="form"]').should("exist")
+
+    cy.get('input#email')
+      .should('be.visible')
+      .should('have.attr', 'placeholder', 'Enter your email')
+      .type('admin@store.com').should('have.value', 'admin@store.com')
+
+    cy.get('input#password')
+      .should('be.visible')
+      .should('have.attr', 'placeholder', 'Enter your password')
+      .type('123456').should('have.value', '123456') 
       
-      // 3) Tunggu dan menulis "Dessert" pada placeholder yang ada
-      // eslint-disable-next-line no-undef
-      cy.get('input[placeholder="Coffee"]', { timeout: 10000 }) // Ganti selector sesuai placeholder yang tepat
-        .should('be.visible')
-        .type('Dessert')
-        .should('have.value', 'Dessert')
-  
-      // 4) Klik tombol Send
-      // eslint-disable-next-line no-undef
-      cy.get('button[type="submit"]').click()
-   
-  
-    })
+    cy.get('[data-testid="submit"]').click()
+    cy.wait(3000); // Tunggu 3 detik setelah klik submit
+
+    cy.get('div.home').should('be.visible')
+    cy.get('div.sidebar').should('be.visible')
+
+    // 1) Mengklik menu Categories dan memastikan masuk ke halaman Categories
+    cy.get('[data-testid="categories"]', { timeout: 10000 }).should('be.visible').click()
+    cy.wait(3000); // Tunggu 3 detik setelah mengklik Categories
+    cy.url().should('include', '/categories')
+    cy.get('div.datatableTitle').contains('CATEGORIES')
+
+    // 2) Klik tombol Add New 
+    cy.get('[data-testid="add-new"]').click() // Ganti selector sesuai dengan tombol Add New
+    cy.wait(2000); // Tunggu 2 detik setelah mengklik Add New
+    
+    // 3) Tunggu dan menulis "Dessert" pada placeholder yang ada
+    cy.get('input[placeholder="Coffee"]', { timeout: 10000 }) // Ganti selector sesuai placeholder yang tepat
+      .should('be.visible')
+      .type('Dessert')
+      .should('have.value', 'Dessert')
+
+    // 4) Klik tombol Send
+    cy.get('button[type="submit"]').click()
   })
+})
--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,19 +1,18 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import {getFirestore} from "firebase/firestore";
+import {getStorage} from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAJRK4tOTCcdY-Gy6Pglf3mIqYX89pceVY",
-    authDomain: "store-tutorial-c250b.firebaseapp.com",
-    projectId: "store-tutorial-c250b",
-    storageBucket: "store-tutorial-c250b.appspot.com",
-    messagingSenderId: "473704181643",
-    appId: "1:473704181643:web:b3cdca9b98fb9f6748098e"
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "store-tutorial-76275.firebaseapp.com",
+  projectId: "store-tutorial-76275",
+  storageBucket: "store-tutorial-76275.appspot.com",
+  messagingSenderId: "677901370009",
+  appId: "1:677901370009:web:71a22687d37759f3376e08",
+  measurementId: "G-KQBQ57D3N3"
 };
-
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth();
-export const db = getFirestore(app);
-export const storage = getStorage(app);
